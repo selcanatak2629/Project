d6c14199ce50222d4dfcf1e7f2a2c6da625ee720
selcanatak2629 <selcanatak2629@gmail.com>
2021-09-03 15:28:59 +0300

    refactor

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -10,9 +10,14 @@ export class CardComponent implements OnInit {
   constructor() { }
  
   products:any[]=[
-    {id:1, name:" otel adı 1",image:"assets/images/image1.jpg",location:"mugla" },
-    {id:2, name:" otel adı 2" ,image:"assets/images/image2.jpg",location:"adana" },
-    {id:3, name:" otel adı 3" ,image:"assets/images/image6.jpg",location:"izmir" }
+    {id:1, name:" Marcan Beach Hotel",image:"assets/images/MarcanBeachHotel.jpeg",location:"Muğla / Fethiye / Ölüdeniz" },
+    {id:2, name:" SuneoClub Mersoy Bellavista" ,image:"assets/images/SuneoClubMersoyBellavista.jpeg",location:"Muğla / Marmaris / Marmaris İçmeler" },
+    {id:3, name:" Royal Asarlık Beach Hotel Spa" ,image:"assets/images/RoyalAsarlıkBeachHotelSpa.jpeg",location:"Muğla / Bodrum / Gümbet" },
+    {id:4, name:" Grand Efe Otel" ,image:"assets/images/GrandEfeOtel.jpeg",location:"İzmir / Menderes / Özdere" },
+    {id:5, name:" Horus Paradise Luxury Resort" ,image:"assets/images/HorusParadiseLuxuryResort.jpeg",location:"Antalya / Side / Kumköy" },
+    {id:6, name:" Taksim International Abant Palace" ,image:"assets/images/TaksimInternationalAbantPalace.jpeg",location:"Bolu / Abant / Abant Göl Kenarı" },
+    {id:7, name:" Senza Grand Santana Hotel" ,image:"assets/images/SenzaGrandSantanaHotel.jpg",location:"Antalya / Alanya / Mahmutlar" }
+
 
   ]
   ngOnInit(): void {
